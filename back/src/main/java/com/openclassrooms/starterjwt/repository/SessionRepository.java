edeeb9640961bb9a846871281e711a61356285c2
Emmanuel Hesry <ehesry@gmail.com>
2026-07-17 19:32:42 +0200

    Remove useless @repository annotations

--- back/src/main/java/com/openclassrooms/starterjwt/repository/SessionRepository.java
+++ back/src/main/java/com/openclassrooms/starterjwt/repository/SessionRepository.java
@@ -2,8 +2,6 @@ package com.openclassrooms.starterjwt.repository;
 
 import com.openclassrooms.starterjwt.models.Session;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface SessionRepository extends JpaRepository<Session, Long> {
 }
--- back/src/main/java/com/openclassrooms/starterjwt/repository/UserRepository.java
+++ back/src/main/java/com/openclassrooms/starterjwt/repository/UserRepository.java
@@ -2,11 +2,8 @@ package com.openclassrooms.starterjwt.repository;
 
 import com.openclassrooms.starterjwt.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 
