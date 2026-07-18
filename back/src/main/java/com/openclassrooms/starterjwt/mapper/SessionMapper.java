9799111e96f9f8ffc9e5e02088411e203de2860c
Emmanuel Hesry <ehesry@gmail.com>
2026-07-18 14:17:23 +0200

    Remove useless @mappings annotation

--- back/src/main/java/com/openclassrooms/starterjwt/mapper/SessionMapper.java
+++ back/src/main/java/com/openclassrooms/starterjwt/mapper/SessionMapper.java
@@ -7,7 +7,6 @@ import com.openclassrooms.starterjwt.services.TeacherService;
 import com.openclassrooms.starterjwt.services.UserService;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Mappings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -17,7 +16,8 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Component
-@Mapper(componentModel = "spring", uses = {UserService.class}, imports = {Arrays.class, Collectors.class, Session.class, User.class, Collections.class, Optional.class})
+@Mapper(componentModel = "spring", uses = { UserService.class }, imports = { Arrays.class, Collectors.class,
+        Session.class, User.class, Collections.class, Optional.class })
 public abstract class SessionMapper implements EntityMapper<SessionDto, Session> {
 
     @Autowired
@@ -25,18 +25,13 @@ public abstract class SessionMapper implements EntityMapper<SessionDto, Session>
     @Autowired
     UserService userService;
 
-    @Mappings({
-            @Mapping(source = "description", target = "description"),
-            @Mapping(target = "teacher", expression = "java(sessionDto.getTeacher_id() != null ? this.teacherService.findById(sessionDto.getTeacher_id()) : null)"),
-            @Mapping(target = "users", expression = "java(Optional.ofNullable(sessionDto.getUsers()).orElseGet(Collections::emptyList).stream().map(user_id -> { User user = this.userService.findById(user_id); if (user != null) { return user; } return null; }).collect(Collectors.toList()))"),
-    })
+    @Mapping(source = "description", target = "description")
+    @Mapping(target = "teacher", expression = "java(sessionDto.getTeacher_id() != null ? this.teacherService.findById(sessionDto.getTeacher_id()) : null)")
+    @Mapping(target = "users", expression = "java(Optional.ofNullable(sessionDto.getUsers()).orElseGet(Collections::emptyList).stream().map(user_id -> { User user = this.userService.findById(user_id); if (user != null) { return user; } return null; }).collect(Collectors.toList()))")
     public abstract Session toEntity(SessionDto sessionDto);
 
-
-    @Mappings({
-            @Mapping(source = "description", target = "description"),
-            @Mapping(source = "session.teacher.id", target = "teacher_id"),
-            @Mapping(target = "users", expression = "java(Optional.ofNullable(session.getUsers()).orElseGet(Collections::emptyList).stream().map(u -> u.getId()).collect(Collectors.toList()))"),
-    })
+    @Mapping(source = "description", target = "description")
+    @Mapping(source = "session.teacher.id", target = "teacher_id")
+    @Mapping(target = "users", expression = "java(Optional.ofNullable(session.getUsers()).orElseGet(Collections::emptyList).stream().map(u -> u.getId()).collect(Collectors.toList()))")
     public abstract SessionDto toDto(Session session);
 }
