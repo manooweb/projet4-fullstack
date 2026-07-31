63b2991d78450082d42e8588d4424d6f45b11e7d
Emmanuel Hesry <emmanuel.hesry@nantesmetropole.fr>
2026-07-31 13:55:49 +0200

    test: assert the application configuration loads in the context

--- back/src/test/java/com/openclassrooms/starterjwt/integration/SpringBootSecurityJwtApplicationIT.java
+++ back/src/test/java/com/openclassrooms/starterjwt/integration/SpringBootSecurityJwtApplicationIT.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 
+import com.openclassrooms.starterjwt.SpringBootSecurityJwtApplication;
+
 @SpringBootTest(properties = {
         "oc.app.jwtSecret=B05rhIFhM+X6AeloFsPDBWdau6FuUwXK7sk08rjUN1lKEUdpXWHkOBHzXO8xYHyc98L2z78uu6h+W3+urd2Nlw==",
         "spring.docker.compose.enabled=false"
@@ -19,6 +21,6 @@ class SpringBootSecurityJwtApplicationIT extends IntegrationTestSupport {
     @Test
     void contextLoads() {
         // Vérifie que le contexte de l'application se charge correctement
-        assertThat(applicationContext).isNotNull();
+        assertThat(applicationContext.getBean(SpringBootSecurityJwtApplication.class)).isNotNull();
     }
 }
