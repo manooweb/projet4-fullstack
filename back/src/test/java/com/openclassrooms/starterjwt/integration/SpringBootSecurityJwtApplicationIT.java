4655701bab3f341d03a5d761989b9c13af5a4900
Emmanuel Hesry <emmanuel.hesry@nantesmetropole.fr>
2026-07-31 15:17:48 +0200

    Remove @SpringBootTest annotation already exists in the abstract class

--- back/src/test/java/com/openclassrooms/starterjwt/integration/SpringBootSecurityJwtApplicationIT.java
+++ back/src/test/java/com/openclassrooms/starterjwt/integration/SpringBootSecurityJwtApplicationIT.java
@@ -4,15 +4,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 
 import com.openclassrooms.starterjwt.SpringBootSecurityJwtApplication;
 
-@SpringBootTest(properties = {
-        "oc.app.jwtSecret=B05rhIFhM+X6AeloFsPDBWdau6FuUwXK7sk08rjUN1lKEUdpXWHkOBHzXO8xYHyc98L2z78uu6h+W3+urd2Nlw==",
-        "spring.docker.compose.enabled=false"
-})
 class SpringBootSecurityJwtApplicationIT extends IntegrationTestSupport {
 
     @Autowired
