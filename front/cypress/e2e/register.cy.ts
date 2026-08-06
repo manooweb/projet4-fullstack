8d07671a03d250e3fc98968a45386eb7860baa56
Emmanuel Hesry <ehesry@gmail.com>
2026-08-06 07:36:55 +0200

    test(e2e): add a test to cover register fails

--- front/cypress/e2e/register.cy.ts
+++ front/cypress/e2e/register.cy.ts
@@ -23,4 +23,31 @@ describe('Register spec', () => {
     cy.url().should('include', '/login');
     cy.contains('mat-card-title', 'Login').should('be.visible');
   });
+
+  it('Register fails', () => {
+    cy.intercept('POST', '/api/auth/register', {
+      statusCode: 400,
+      body: {
+        message: 'Error: Email is already taken!'
+      }
+    }).as('register');
+
+    cy.visit('/register');
+
+    cy.get('input[formControlName=firstName]').type("firstName");
+    cy.get('input[formControlName=lastName]').type("lastName");
+    cy.get('input[formControlName=email]').type("existing.email@studio.com");
+    cy.get('input[formControlName=password]').type(`${"test!1234"}`);
+
+    cy.contains('button', 'Submit').click();
+
+    cy.wait('@register').its('response')
+      .should( response => {
+        expect(response).to.have.property('statusCode', 400);
+        expect(response.body).to.have.property('message', 'Error: Email is already taken!');
+      });
+
+    cy.url().should('include', '/register');
+    cy.contains('p.error', 'An error occurred').should('be.visible');
+  });
 });
