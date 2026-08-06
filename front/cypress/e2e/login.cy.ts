a7f7fd8a6db397d390734e94540aa82c95f4b1ca
Emmanuel Hesry <ehesry@gmail.com>
2026-08-06 07:15:11 +0200

    test(e2e): add a test to cover login fails

--- front/cypress/e2e/login.cy.ts
+++ front/cypress/e2e/login.cy.ts
@@ -3,6 +3,7 @@ describe('Login spec', () => {
     cy.visit('/login');
 
     cy.intercept('POST', '/api/auth/login', {
+      statusCode: 200,
       body: {
         id: 1,
         username: 'yoga@studio.com',
@@ -19,14 +20,57 @@ describe('Login spec', () => {
       },
       []);
 
-    
-    cy.get('input[formControlName=email]').clear();
-    cy.get('input[formControlName=password]').clear();
-    cy.get('input[formControlName=email]').type("yoga@studio.com");
-    cy.get('input[formControlName=password]').type(`${"test!1234"}`);
+    cy.get('input[formControlName=email]').clear().type("yoga@studio.com");
+    cy.get('input[formControlName=password]').clear().type(`${"test!1234"}`);
 
     cy.get('button[type=submit]').click();
 
     cy.url().should('include', '/sessions');
+  });
+
+  it('Login fails', () => {
+    cy.visit('/login');
+
+    cy.intercept('POST', '/api/auth/login', {
+      statusCode: 401,
+      body: {
+        message: 'Invalid credentials'
+      }
+    }).as('login');
+
+    cy.intercept(
+      {
+        method: 'GET',
+        url: '/api/session',
+      },
+      []);
+
+
+    cy.get('input[formControlName=email]').clear().type("yoga@studio.com");
+    cy.get('input[formControlName=password]').clear().type(`${"wrongpassword"}`);
+
+    cy.get('button[type=submit]').click();
+
+    cy.wait('@login')
+      .its('response')
+      .should('have.property', 'statusCode', 401);
+
+    cy.url().should('include', '/login');
+    cy.contains('p.error', 'An error occurred').should('be.visible');
+  });
+
+  it('Should toggle password visibility when the toggle button is clicked once', () => {
+    cy.visit('/login');
+
+    cy.get('input[formControlName=email]')
+      .clear()
+      .type('yoga@studio.com');
+
+    cy.get('input[formControlName=password]')
+      .clear()
+      .type('test!1234');
+
+    cy.get('button[mat-icon-button]').click();
+    cy.get('input[formControlName=password]').should('have.attr', 'type', 'text');
   });
 });
