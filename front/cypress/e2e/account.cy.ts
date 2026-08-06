ec6dda64100d365b3c19256797eae73a04832d29
Emmanuel Hesry <ehesry@gmail.com>
2026-08-06 09:06:36 +0200

    test(e2e): add account back button test

--- front/cypress/e2e/account.cy.ts
+++ front/cypress/e2e/account.cy.ts
@@ -198,4 +198,131 @@ describe('Account spec', () => {
     cy.url().should('include', '/login');
     cy.contains('mat-card-title', 'Login').should('be.visible');
   });
+
+  it('Should not display delete button when the user is authenticated as an admin user', () => {
+    const userSession = {
+      token: 'fake-jwt-admin',
+      type: 'Bearer',
+      id: 1,
+      username: 'test@studio.com',
+      firstName: 'Test',
+      lastName: 'Demo',
+      admin: true
+    };
+
+    const accountResponse = {
+      id: 1,
+      email: 'test@studio.com',
+      firstName: 'Test',
+      lastName: 'Demo',
+      admin: true,
+      password: '',
+      createdAt: '2026-01-10T10:00:00.000Z',
+      updatedAt: '2026-02-10T10:00:00.000Z'
+    }
+    cy.visit('/login');
+
+    cy.intercept('POST', '/api/auth/login', {
+      statusCode: 200,
+      body: userSession,
+    }).as('login');
+
+    cy.intercept(
+      {
+        method: 'GET',
+        url: '/api/session',
+      },
+      []).as('sessions');
+
+
+    cy.get('input[formControlName=email]').clear().type("yoga@studio.com");
+    cy.get('input[formControlName=password]').clear().type(`${"test!1234"}`);
+
+    cy.get('button[type=submit]').click();
+
+    cy.wait('@login');
+    cy.wait('@sessions');
+
+    cy.intercept(
+      {
+        method: 'GET',
+        url: '/api/user/1',
+      },
+      {
+        statusCode: 200,
+        body: accountResponse
+      }).as('me');
+
+    cy.get('span.link').contains('Account').click();
+
+    cy.url().should('include', '/me');
+    cy.contains('h1', 'User information').should('be.visible');
+    cy.contains('p', 'Name: Test DEMO').should('be.visible');
+    cy.contains('p', 'Email: test@studio.com').should('be.visible');
+    cy.contains('p', 'You are admin').should('be.visible');
+    cy.contains('p', 'Delete my account:').should('not.exist');
+    cy.contains('button', 'Delete').should('not.exist');
+  });
+
+  it('Should return back to sessions list when clicking the back button on account page', () => {
+    const userSession = {
+      token: 'fake-jwt-user',
+      type: 'Bearer',
+      id: 1,
+      username: 'test@studio.com',
+      firstName: 'Test',
+      lastName: 'Demo',
+      admin: false
+    };
+
+    const accountResponse = {
+      id: 1,
+      email: 'test@studio.com',
+      firstName: 'Test',
+      lastName: 'Demo',
+      admin: false,
+      password: '',
+      createdAt: '2026-01-10T10:00:00.000Z',
+      updatedAt: '2026-02-10T10:00:00.000Z'
+    }
+    cy.visit('/login');
+
+    cy.intercept('POST', '/api/auth/login', {
+      statusCode: 200,
+      body: userSession,
+    }).as('login');
+
+    cy.intercept(
+      {
+        method: 'GET',
+        url: '/api/session',
+      },
+      []).as('sessions');
+
+
+    cy.get('input[formControlName=email]').clear().type("yoga@studio.com");
+    cy.get('input[formControlName=password]').clear().type(`${"test!1234"}`);
+
+    cy.get('button[type=submit]').click();
+
+    cy.wait('@login');
+    cy.wait('@sessions');
+
+    cy.intercept(
+      {
+        method: 'GET',
+        url: '/api/user/1',
+      },
+      {
+        statusCode: 200,
+        body: accountResponse
+      }).as('me');
+
+    cy.get('span.link').contains('Account').click();
+
+    cy.contains('button mat-icon', 'arrow_back').click();
+
+    cy.url().should('include', '/sessions');
+    cy.contains('mat-card-title', 'Sessions available').should('be.visible');
+  });
 });
